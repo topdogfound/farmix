5e1292743b3f9142afcef212d1c9ac1f6477ad67
topdogfound <topdogfound@gmail.com>
2025-12-18 12:13:52 +0530

    Installed filament and implemented admin pannel

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected string $view = 'filament.pages.dashboard';
+
+}
